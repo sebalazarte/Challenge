9f6274b2f2009fe802a7db2168800249d7e6095e
Andres Sebastian Lazarte <sebalazarte@gmail.com>
2022-01-03 11:02:50 -0300

    cambio en docker file

--- Challenge/Repositories/MongoDBRepository.cs
+++ Challenge/Repositories/MongoDBRepository.cs
@@ -9,7 +9,7 @@ namespace Challenge.Repositories
 
         public MongoDBRepository()
         {
-            client = new MongoClient("mongodb://localhost:27017");
+            client = new MongoClient("mongodb+srv://slazarte:48006321Benja@miclustercafe.n5ymv.mongodb.net");
             db = client.GetDatabase("PromocionesDB");
         }
     }
